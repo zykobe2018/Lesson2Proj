04d4346d5068c026be57bc8a009b0752381318d6
zykobe2018 <1119553547@qq.com>
2018-05-13 17:22:58 +0800

    1.新建了一个Lesson2Proj项目，测试提交是否成功。 2.使用的默认的布局ConstrantLayout

---
+++ app/src/main/java/com/zy/lesson2proj/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zy.lesson2proj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
